59dffefa4751c4d8a628550a75e00b7c559d79a2
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-04-04 11:33:59 +0300

    Add remaining code of the calculator

--- src/main/java/ee/ut/graafika/Kalkulaator.java
+++ src/main/java/ee/ut/graafika/Kalkulaator.java
@@ -2,6 +2,8 @@ package ee.ut.graafika;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -26,6 +28,29 @@ public class Kalkulaator extends Application {
 			"0", ".", "=", "+"
 		};
 		
+		for (int i = 0; i < buttonLabels.length; i++) {
+			Button button = new Button(buttonLabels[i]);
+			button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			GridPane.setConstraints(button, i % 4, 1 + i / 4);
+			
+			button.setOnAction(event -> {
+				Button source = (Button) event.getSource();
+				String buttonText = source.getText();
+				if (buttonText.equals("=")) {
+					// arvutamine
+				} else {
+					display.setText(buttonText);
+				}
+			});
+			
+			gridPane.getChildren().add(button);
+		}
+		
+		gridPane.getChildren().add(display);
+		
+		Scene scene = new Scene(gridPane, 300, 300);
+		
+		peaLava.setScene(scene);
 		peaLava.setTitle("Kalkulaator");
 		peaLava.show();
 	}
